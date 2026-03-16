2b4a60bbaf5f2cd18f0a874931bc66876ffbe983
Nick Fedorenko <nfm12121994@gmail.com>
2026-03-16 15:26:21 +0300

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -1,217 +1,225 @@
-﻿import sys
-import os.path
+﻿from typing import List
 
 import allure
-import psutil
 import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-
-from datetime import datetime
+from selenium.common import NoAlertPresentException
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 from base_page.base_page import BasePage
-from config import Options as optionConfig
-from PIL import Image
-import io
 
+from components.dcad_components.authorization_dcad_page import AuthorizationDcadPage
+from components.dcad_components.header_dcad_component import HeaderDcadComponent
+from components.open_components.authorization_modal import AuthorizationModal
+from components.open_components.cookie_component import CookieComponentModal
+from components.open_components.header_component.nav_bar_ridan import NavBarRidan
+from config import TestEnvironment
+from crm_pages.athorization_page.authorization_page import AuthorizationPage
+from crm_pages.header_page.header_page import HeaderComponent
+from crm_pages.pq_page.pq_page import PqPage
+from crm_pages.pq_page.pq_page_locators import PqPageLocators
 from tools import helper
 
-sys.stdout.reconfigure(encoding='utf-8')
+
+@pytest.fixture
+def authorization_open_fixture(browser: WebDriver):
+    def _authorization_open(login: str, password: str):
+        """
+        Авторизация в Опен
+        :param login: Логин
+        :param password: Пароль
+        """
+        nav_bar_ridan = NavBarRidan(browser)
+        authorization_modal = AuthorizationModal(browser)
+        cookie_component = CookieComponentModal(browser)
+
+        nav_bar_ridan.btn_to_come_in.click()
+        authorization_modal.should_header_visible()
+        authorization_modal.filling_email(login)
+        authorization_modal.filling_password(password)
+        authorization_modal.click_btn_come_in()
+
+        cookie_component.click_btn_i_see_cookie()
+
+    return _authorization_open
 
 
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    """Делаем скрины только при падении тестов"""
-    outcome = yield
-    rep = outcome.get_result()
-    if rep.when == 'call' and rep.failed:
-        test_name = os.environ.get('PYTEST_CURRENT_TEST'
-                                   ).split('/')[-1].split('::')[0]  # получение имени запускаемого теста
-        if optionConfig.PATH_TO_SCREENSHOT is not None:
-            if os.path.exists(optionConfig.PATH_TO_SCREENSHOT):
-                now = datetime.now()
-                now_data = now.strftime("%d.%m.%y.%H.%M.%S")
-                name_screenshot = f'{now_data}_{test_name}.png'
-                try:
-                    if 'browser' in item.fixturenames:  # Получаем экземпляр драйвера
-                        web_driver = item.funcargs['browser']
-                    else:
-                        print('Fail to take screen-shot')
-                        return
+@pytest.fixture
+def authorization_dcad_fixture(browser: WebDriver):
+    def _authorization_dcad(login: str, password: str):
+        """
+        Авторизация в DCAD
+        :param login: Email для входа
+        :param password: Пароль
+        """
+        header_dcad = HeaderDcadComponent(browser)
+        authorization_page = AuthorizationDcadPage(browser)
 
-                    # Для скрина в отчете allure
-                    attach = web_driver.get_screenshot_as_png()
-                    allure.attach(attach, name_screenshot, attachment_type=allure.attachment_type.PNG)
+        # Проверяем, не авторизован ли уже пользователь
+        if header_dcad.is_user_logged_in():
+            print(f'Пользователь уже авторизован')
+            return
 
-                    path = optionConfig.PATH_TO_SCREENSHOT + name_screenshot
-                    screen(web_driver, path)
-                except Exception as e:
-                    print("error on write screenshot")
-                    print(optionConfig.PATH_TO_SCREENSHOT + name_screenshot)
-                    print(e, '- ошибка')
+        # Проверяем что форма авторизации открылась
+        authorization_page.should_header_visible()
+
+        # Заполняем форму
+        authorization_page.filling_email(login)
+        authorization_page.filling_password(password)
+
+        # Отправляем форму
+        authorization_page.click_btn_submit()
+        header_dcad.wait_user_logged_in()
+
+    return _authorization_dcad
 
 
-def screen(browser, path):
-    """Скриншот экрана не только видимой части экрана (со скролом страницы)"""
+@pytest.fixture
+def emergence_of_different_types_of_agreements_in_kp(browser):
+    def _emergence_of_different_types_of_agreements(
+            id_pq: str,
+            num_pq: str,
+            type_of_agreement_visible_after_save: list,
+            type_of_agreement_visible_after_save_after_change_condition: list,
+            delivery_type: str,
+            expected_status_agreement: dict
+    ):
+        """
+        Действия по проверке возникновения согласований в разных видах КП.
+        С бесплатной доставкой на адрес, условия оплаты - согласование условий оплаты
+        :param id_pq: id КП
+        :param num_pq: номер КП
+        :param type_of_agreement_visible_after_save: Ожидаемые типы согласований после сохранения
+        :param type_of_agreement_visible_after_save_after_change_condition:  Ожидаемые типы согласований после изменений условий в КП
+        :param delivery_type: Вид доставки (Доставка на указанный адрес, стандартные договорные условия)
+        :param expected_status_agreement: Ожидаемые статусы у согласований
+        :return: Функция, в которой действия по проверке возникновения типов согласований в разных видах КП
+        """
 
-    total_height = browser.execute_script("return document.body.scrollHeight")
-    size = browser.get_window_size()
-    screenshot = Image.new('RGB', (size['width'], total_height))
+        link_pq = TestEnvironment.LINK_PQ_IN_CRM
+        link_pq_total = link_pq + id_pq
+        page_pq = PqPage(browser, link_pq_total)
+        # Открываем КП в срм
+        page_pq.open()
+        page_pq.click_btn_save_pq()
+        page_pq.check_visible_type_of_agreement(type_of_agreement_visible_after_save, num_pq)
+        page_pq.switching_currency_to_a_fixed_rate()
+        page_pq.selection_of_delivery_conditions(delivery_type)
+        page_pq.choice_of_free_payment_mode()
+        page_pq.expanding_payment_terms_menu()
+        page_pq.click_on_button_agree_on_payment_terms()
+        page_pq.click_btn_save_pq()
+        page_pq.check_visible_type_of_agreement(type_of_agreement_visible_after_save_after_change_condition, num_pq)
+        page_pq.send_for_approval_pq_page()
+        page_pq.status_of_checkpoint_must_be_approval()
 
-    offset = 0
-    while offset < total_height:
-        browser.execute_script("window.scrollTo(0, %s);" % offset)
-        screenshot.paste(Image.open(io.BytesIO(browser.get_screenshot_as_png())), (0, offset))
-        # offset += 1080 # Вы можете изменить эту величину в зависимости от вашего экрана
-        offset += size['height']  # Вы можете изменить эту величину в зависимости от вашего экрана
-    screenshot.save(path, format='png')
-
-
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default='chrome',
-                     help="Choose driver: chrome or firefox")
-
-
-def get_cpu_usage():
-    # Получаем загрузку ЦП в процентах
-    cpu_usage = psutil.cpu_percent(interval=1)  # Интервал в секундах
-    print(f"Загруженность ЦП: {cpu_usage}%")
-
-
-def get_memory_usage():
-    # Получаем информацию о памяти
-    memory = psutil.virtual_memory()
-    memory_total = memory.total / (1024 ** 3)  # Перевод в гигабайты
-    memory_used = memory.used / (1024 ** 3)  # Перевод в гигабайты
-    memory_percent = memory.percent  # Процент использования
-
-    print(f"Общая память: {memory_total:.2f} ГБ")
-    print(f"Используемая память: {memory_used:.2f} ГБ")
-    print(f"Загрузка памяти: {memory_percent}%")
-
-
-def pytest_runtest_setup(item):
-    """Получаем все маркировки теста"""
-    markers = item.iter_markers()
-
-    # for marker in markers:
-    # print(f'Запуск теста "{item.name}" с маркировкой: {marker.name}')
-    return markers
+    return _emergence_of_different_types_of_agreements
 
 
-def get_markers(request) -> list:
-    """
-    Получение маркировок в тесте во время запуска
-    :return: Список маркировок, используемых в тесте во время запуска
-    """
-    markers = request.node.iter_markers()
-    markers = [marker.name for marker in markers]
-    return markers
+@pytest.fixture
+def auth_crm_extru_start(browser):
+    def _auth_start(ruco='RUCO1681', password='1234'):
+        """
+            Авторизация в срм при старте теста.
+            По умолчанию используется пользователь:
+            - ruco: RUCO1681 (Оганесян Левон)
+            - Пароль: 1234
+            :param ruco: ruco пользователя (логин)
+            :param password: Пароль
+            :return: _auth_start - функция, выполняющая авторизацию в срм
+        """
+        with allure.step(f'Авторизация в срм. Логин - {ruco}, пароль - {password}'):
+            link_auth = TestEnvironment.LINK_CRM_TST_AUTHORIZATION
+            page_auth = AuthorizationPage(browser, link_auth)
+
+            page_auth.open()
+            page_auth.authorization_extru(login=ruco, password=password)
+
+    return _auth_start
 
 
-# для выгрузки всех артикулов из параметризации тестов. выгружаются даже просто при запуске pytest --collect-only
-# def pytest_collection_modifyitems(config, items):
-#     # Собираем артикулы из параметров тестов
-#     articles = set()
-#
-#     for item in items:
-#         # pytest хранит параметры в атрибуте callspec (если есть)
-#         if hasattr(item, "callspec"):
-#             # callspec.params — словарь параметров
-#             params = item.callspec.params
-#             # Предположим, параметр называется "article"
-#             if "article" in params:
-#                 articles.add(str(params["article"]))
-#         else:
-#             # Если параметризации нет — пропускаем
-#             pass
-#
-#     # Сохраняем
-#     with open("collected_articles.txt", "w") as f:
-#         for art in sorted(articles):
-#             f.write(art + "\n")
-#
-#     print(f"\n✅ Найдено {len(articles)} артикулов. Сохранены в collected_articles.txt")
-#     # Опционально: принудительно завершить сбор, не запуская тесты
-#     config.option.collectonly = True  # уже в режиме сбора, но можно явно
+@pytest.fixture
+def auth_extru_crm_from_authorized_page(browser):
+    """Авторизация в экстру срм с уже авторизованной страницы"""
+
+    def _auth_extru(url_page: str, name_user='RUCO1681', password='1234'):
+        """
+        Авторизация в экстру срм с уже авторизованной страницы
+        :param url_page:  url страницы, с которой будет авторизация
+        :param name_user: Имя пользователя. Для дальнейшего определения ruco
+        :return: Функция, которая проходит авторизацию в срм со страницы срм
+        """
+        with allure.step(
+                f'Авторизация в экстру срм с уже авторизованной страницы {url_page}. Пользователь - {name_user}'):
+            page_pq = PqPage(browser, url=url_page)
+            page_header = HeaderComponent(browser)
+            page_auth = AuthorizationPage(browser, TestEnvironment.LINK_CRM_TST_AUTHORIZATION)
+
+            cookie_session = page_pq.get_cookie_session()
+            ruco = helper.get_ruco_user_crm_by_name(cookie=cookie_session, name_user=name_user)
+            print(ruco)
+            helper.release_all_substitutions_crm(cookie_session)
+            page_header.activate_profile_menu()
+            page_header.btn_logout.click()
+
+            page_auth.open()
+            page_auth.authorization_extru(login=ruco, password=password)
+
+            page_pq.open()
+
+    return _auth_extru
 
 
-@pytest.fixture(scope='function')
-def browser(request):
-    browser_name = request.config.getoption('browser_name')
-    options_add_experimental_option_detach = True  # не закрывать окно браузера по завершении теста (True - не закрывать)
-    driver = None
+@pytest.fixture
+def agreement_on_all_conditions_extru(browser, auth_extru_crm_from_authorized_page):
+    """Согласование всех условий на внешнем сайте extru"""
 
-    markers = get_markers(request)
-    prod_marker = 'prod'  # Маркировка для тестов на проде
+    def _agreement_all():
+        with allure.step('Согласование всех условий на внешнем сайте extru'):
+            link_pq = browser.current_url
+            page_base = BasePage(browser, link_pq)
 
-    if browser_name == 'chrome':
-        get_cpu_usage()
-        get_memory_usage()
+            page_base.waiting_element_is_visibility_located_dom(PqPageLocators.LOCATOR_CONSOLIDATION_CHECKBOX, sec=30)
+            count_consolidation_checkbox = browser.find_elements(
+                *PqPageLocators.LOCATOR_CONSOLIDATION_CHECKBOX)  # количество согласований
+            while len(count_consolidation_checkbox) != 0:
 
-        headless = 0  # Безголовый режим 0 - false
-        user_agent = 'RIDAN_RND_TEST'
+                pq_page = PqPage(browser, url=link_pq)
 
-        options = Options()
-        options.add_argument("--disable-notifications")
-        options.add_argument("--disable-infobars")  # отключить нативные уведомления браузера
-        if prod_marker in markers:
-            options.add_argument(f"user-agent={user_agent}")
-        options.add_argument('--no-sandbox')
-        options.add_argument("--log-level=3")
-        options.add_argument(
-            "--password-store=basic")  # отключает интеграцию с системным менеджером паролей (в т.ч. Google Password Manager).
+                count_warning = browser.find_elements(*PqPageLocators.LOCATOR_ORANGE_AGREEMENT_FLAGS
+                                                      )  # Согласователи с оранжевой меткой
+                if len(count_warning) != 0:
+                    conciliator_list = pq_page.agreement_with_orange_mark()
 
-        # Отключает встроенное уведомление об утечке паролей
-        options.add_argument("--disable-password-leak-detection")
-        options.add_argument("--disable-sync")  # Отключает синхронизацию с Google
+                else:
+                    conciliator_list = pq_page.agreement_with_blue_mark()
 
-        options.add_experimental_option('excludeSwitches', ['enable-logging', '--dns-prefetch-disable'])
+                print(conciliator_list)
 
-        options.add_experimental_option("detach", options_add_experimental_option_detach
-                                        )  # не закрывать окно браузера по завершении теста (True - не закрывать)
-        prefs = {
-            "profile.password_manager_leak_detection": False,
-            'profile.default_content_setting_values.automatic_downloads': 1,
-            'plugins.always_open_pdf_externally': True,  # It will not show PDF directly in chrome
-            'profile.password_manager_enabled': False,  # Отключить менеджер паролей в хром
-            'credentials_enable_service': False,  # Отключает службу сохранения и автозаполнения логинов/паролей,
-            'profile.default_content_setting_values.notifications': 2,  # отключить уведомления
-        }
-        options.add_experimental_option("prefs", prefs)  # разрешение на скачивание нескольких файлов
+                auth_extru_crm_from_authorized_page(link_pq, conciliator_list[0])
+                pq_page.reconciliation_of_outdated_costs()
+                with allure.step('Клик по галочке согласование напротив согласователя'):
+                    page_base.waiting_element_is_visibility_located_dom(PqPageLocators.LOCATOR_AGREEMENT_TRUE, sec=15)
+                    agreement_true = page_base.find_element(PqPageLocators.LOCATOR_AGREEMENT_TRUE)
+                    page_base.scroll_to(agreement_true)
+                    page_base.expecting_clickability(PqPageLocators.LOCATOR_AGREEMENT_TRUE, sec=10)
 
-        options.set_capability('goog:loggingPrefs', {'performance': 'ALL'})
+                    agreement_true = page_base.find_element(PqPageLocators.LOCATOR_AGREEMENT_TRUE)
+                    agreement_true.click()
+                    page_base.waiting_element_is_visibility_located_dom(PqPageLocators.LOCATOR_H4_AGREEMENT, sec=90)
 
-        if optionConfig.RUN_HEADLESS_BROWSER == '1':
-            # options.add_argument("--headless")
-            options.add_argument("--headless=new")
-            headless = 1  # Безголовый режим 1 - true
+                pq_page.agreement_kp_for_client()
+                pq_page.entering_a_comment_for_approval()
+                pq_page.click_on_the_agree_button()
 
-        print('\nstart chrome driver for test...')
+                count_consolidation_checkbox = browser.find_elements(
+                    *PqPageLocators.LOCATOR_CONSOLIDATION_CHECKBOX)  # количество согласований
 
-        #  При инициализации драйвера через менеджер драйвера
-        # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    return _agreement_all
 
-        # global driver
-        driver = webdriver.Chrome(options=options)
-        # При абсолютном пути драйвера
-        # service = Service(executable_path=r'.\chrome_driver\chromedriver.exe')
-        # driver = webdriver.Chrome(service=service, options=options)
 
-        if headless == 1:
-            driver.set_window_size(width=1920, height=1080)
-        else:
-            driver.maximize_window()
-
-    yield driver
-
-    expected_marker = 'stage'
-    if expected_marker in markers:
-        page_base = BasePage(driver, '')
-        cookie = page_base.get_cookie_session()
-        helper.release_all_substitutions_crm(cookie)
-        print(f'Скинули все подмены при запуске теста с маркировкой {expected_marker}')
-
-    print('\nquit driver..')
-    # driver.close()
-    # driver.quit()
+@pytest.fixture
+def get_article_from_parametrize_test(request: pytest.FixtureRequest) -> List[str]:
+    """Получение артикулов из параметризации(параметр - article)"""
+    article_for_test = request.node.funcargs.get('article')
+    article_for_test = ' '.join(article_for_test).split()[::2]
+    return article_for_test
